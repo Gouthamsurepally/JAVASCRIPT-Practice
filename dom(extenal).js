238df298081e24e0951917c3aa9a66454cd43fc2
Gouthamsurepally <gouthamsurepalli20@gmail.com>
2024-02-08 18:25:12 +0530

    first push

---
+++ dom(extenal).js
@@ -0,0 +1,5 @@
+function goutham() {
+    window.document.images[0].src = "../public/assets/a3.jpg";
+    window.document.forms[0].elements[1].value = "Register"
+    window.document.forms[1].elements[1].value = "Login"
+}
